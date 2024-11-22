e5145c9f88d0c2b3a4ccd2452f60002b0bd0ee56
Ratan <rtpunati@gmail.com>
2024-11-22 17:49:00 -0600

    Deleted another three files

--- scripts/demo.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder \
-    .appName("SparkDataFrameExample") \
-    .config("spark.hadoop.security.authentication", "simple") \
-    .config("spark.hadoop.security.authorization", "false") \
-    .config("spark.sql.warehouse.dir", "file:///C:/tmp") \
-    .getOrCreate()
